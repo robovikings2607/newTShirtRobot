e1da369c89fe604223eab9c644eef7ce384604d9
FRC2607Dev <FRC2607Dev@gmail.com>
2016-08-20 12:11:46 -0400

    created main robot class, currently empty

---
+++ src/edu/archwood/frc2607/newTShirtRobot.java
@@ -0,0 +1,35 @@
+package edu.archwood.frc2607;
+
+import javax.microedition.midlet.MIDletStateChangeException;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+public class newTShirtRobot extends IterativeRobot {
+
+	public void disabledInit() {
+		// TODO Auto-generated method stub
+		super.disabledInit();
+	}
+
+	public void disabledPeriodic() {
+		// TODO Auto-generated method stub
+		super.disabledPeriodic();
+	}
+
+	public void robotInit() {
+		// TODO Auto-generated method stub
+		super.robotInit();
+	}
+
+	public void teleopInit() {
+		// TODO Auto-generated method stub
+		super.teleopInit();
+	}
+
+	public void teleopPeriodic() {
+		// TODO Auto-generated method stub
+		super.teleopPeriodic();
+	}
+
+
+}
