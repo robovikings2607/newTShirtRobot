be337eb5293719f922c408dc54dec0b3c03f7c3f
FRC2607Dev <FRC2607Dev@gmail.com>
2016-09-08 21:02:29 -0400

    Finished drive controls and turntable

--- src/edu/archwood/frc2607/Constants.java
+++ src/edu/archwood/frc2607/Constants.java
@@ -3,4 +3,5 @@ package edu.archwood.frc2607;
 public class Constants {
 static int[] leftMotorPorts = {1,3} ;
 static int[] rightMotorPorts = {2,4} ;
+static int turntableMotorPort = 5 ;
 }
--- src/edu/archwood/frc2607/newTShirtRobot.java
+++ src/edu/archwood/frc2607/newTShirtRobot.java
@@ -5,12 +5,15 @@ import javax.microedition.midlet.MIDletStateChangeException;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Victor;
 
 public class newTShirtRobot extends IterativeRobot {
 	
 	public Transmission transmissionL, transmissionR ;
 	public RobotDrive robotDrive ;
 	public Relay horn ;
+public RobovikingStick joystick ;
+	public Victor turntable ;
 
 	public void disabledInit() {
 		// TODO Auto-generated method stub
@@ -29,6 +32,8 @@ public class newTShirtRobot extends IterativeRobot {
 		transmissionR = new Transmission (Constants.rightMotorPorts) ;
 		robotDrive = new RobotDrive (transmissionL, transmissionR) ;
 		horn = new Relay (7) ;
+		joystick = new RobovikingStick (1) ;
+		turntable = new Victor (Constants.turntableMotorPort) ;
 	}
 
 	public void teleopInit() {
@@ -39,6 +44,14 @@ public class newTShirtRobot extends IterativeRobot {
 	public void teleopPeriodic() {
 		// TODO Auto-generated method stub
 		super.teleopPeriodic();
+		robotDrive.arcadeDrive(-joystick.getRawAxisWithDeadzone(RobovikingStick.xBoxLeftStickY), joystick.getRawAxisWithDeadzone(RobovikingStick.xBoxRightStickX));
+		if (joystick.getRawButton(RobovikingStick.xBoxLeftBumper)){
+			turntable.set(.6); 
+		} else if (joystick.getRawButton(RobovikingStick.xBoxRightBumper)) {
+				turntable.set(-.6);
+		} else {
+			turntable.set(0.0);
+		}
 	}
 
 
